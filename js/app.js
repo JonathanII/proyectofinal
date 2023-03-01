6cc6dbf33415501beed12200b5f1150b6f0bd875
unknown <jonathan2.gomez@gmail.com>
2023-03-01 20:38:59 -0300

    se agregaron funciones de JS a los datos de contacto en la pagina inicial

---
+++ js/app.js
@@ -0,0 +1,29 @@
+document.getElementById('email').onclick = function () {
+    document.getElementById('texto_uno').innerHTML = "Mi correo electronico es"
+    document.getElementById('data').innerHTML = "jonathan2.gomez@gmail.com"
+    document.getElementById('email').style.color = '#EDEDED'; 
+}
+
+document.getElementById('telefono').onclick = function () {
+    document.getElementById('texto_uno').innerHTML = "Mi numero de telefono es"
+    document.getElementById('data').innerHTML = "+54 9 2984 4503063"
+    document.getElementById('telefono').style.color = '#EDEDED';
+}
+
+document.getElementById('linkedin').onclick = function () {
+    document.getElementById('texto_uno').innerHTML = "Mi Linkedin es"
+    document.getElementById('data').innerHTML = "/in/jonathansegundo/"
+    document.getElementById('linkedin').style.color = '#EDEDED';
+}
+
+document.getElementById('github').onclick = function () {
+    document.getElementById('texto_uno').innerHTML = "Mi correo electronico es"
+    document.getElementById('data').innerHTML = "github.com/JonathanII"
+    document.getElementById('github').style.color = '#EDEDED';
+}
+
+document.getElementById('web').onclick = function () {
+    document.getElementById('texto_uno').innerHTML = "Mi pagina web es"
+    document.getElementById('data').innerHTML = "www.miweb.com"
+    document.getElementById('web').style.color = '#EDEDED';
+}
